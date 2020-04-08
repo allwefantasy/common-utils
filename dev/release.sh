daf9fcf21e7f45e38831fcaea13adc4f6b7b887a
WilliamZhu <allwefantasy@gmail.com>
2020-04-08 14:22:49 +0800

    update

---
+++ dev/release.sh
@@ -0,0 +1,5 @@
+mvn clean deploy -DskipTests -Pdisable-java8-doclint -Prelease-sign-artifacts -Pscala-2.11
+
+./dev/change-scala-version 2.12
+mvn clean deploy -DskipTests -Pdisable-java8-doclint -Prelease-sign-artifacts -Pscala-2.12
+git co .
