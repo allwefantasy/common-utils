6fc5795435160a8603a03c150740dfb4d569639d
WilliamZhu <allwefantasy@gmail.com>
2020-04-14 18:53:03 +0800

    make PathFun platform independent

--- src/main/java/tech/mlsql/common/utils/path/PathFun.scala
+++ src/main/java/tech/mlsql/common/utils/path/PathFun.scala
@@ -1,16 +1,19 @@
 package tech.mlsql.common.utils.path
 
+import java.io.File
+
 import scala.collection.mutable.ArrayBuffer
 
 /**
   * 2019-04-25 WilliamZhu(allwefantasy@gmail.com)
   */
 class PathFun(rootPath: String) {
+  private val pathSeparator = File.pathSeparator
   private val buffer = new ArrayBuffer[String]()
-  buffer += rootPath.stripSuffix("/")
+  buffer += rootPath.stripSuffix(pathSeparator)
 
   def add(path: String) = {
-    val cleanPath = path.stripPrefix("/").stripSuffix("/")
+    val cleanPath = path.stripPrefix(pathSeparator).stripSuffix(pathSeparator)
     if (!cleanPath.isEmpty) {
       buffer += cleanPath
     }
@@ -22,7 +25,7 @@ class PathFun(rootPath: String) {
   }
 
   def toPath = {
-    buffer.mkString("/")
+    buffer.mkString(pathSeparator)
   }
 
 }
