f251ee508257da92f45498370ee0983409a0d6ae
WilliamZhu <allwefantasy@gmail.com>
2021-09-14 19:20:10 +0800

    ugprade

--- dev/release.sh
+++ dev/release.sh
@@ -1,7 +1,7 @@
 git co master
-./dev/change-scala-version 2.11
+./dev/change-scala-version.sh 2.11
 mvn clean deploy -DskipTests -Pdisable-java8-doclint -Prelease-sign-artifacts
 
-./dev/change-scala-version 2.12
+./dev/change-scala-version.sh 2.12
 mvn clean deploy -DskipTests -Pdisable-java8-doclint -Prelease-sign-artifacts -Pscala-2.12
 git co .
