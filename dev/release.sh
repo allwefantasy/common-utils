d4abc19f207acb9c8c975a6a45e4a0d707a00bce
WilliamZhu <allwefantasy@gmail.com>
2021-09-30 22:00:50 +0800

    update

--- dev/release.sh
+++ dev/release.sh
@@ -3,5 +3,5 @@ mlsql_plugin_tool  scala211
 mvn clean deploy -DskipTests -Pdisable-java8-doclint -Prelease-sign-artifacts
 
 mlsql_plugin_tool  scala212
-mvn clean deploy -DskipTests -Pdisable-java8-doclint -Prelease-sign-artifacts -Pscala-2.12
+mvn clean deploy -DskipTests -Pdisable-java8-doclint -Prelease-sign-artifacts
 git co .
