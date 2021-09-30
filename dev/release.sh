6707596de1c06203b6c518553824e1339656dac9
WilliamZhu <allwefantasy@gmail.com>
2021-09-30 21:56:09 +0800

    update

--- dev/release.sh
+++ dev/release.sh
@@ -1,7 +1,7 @@
 git co master
-./dev/change-scala-version.sh 2.11
+mlsql_plugin_tool build  scala211
 mvn clean deploy -DskipTests -Pdisable-java8-doclint -Prelease-sign-artifacts
 
-./dev/change-scala-version.sh 2.12
+mlsql_plugin_tool build  scala212
 mvn clean deploy -DskipTests -Pdisable-java8-doclint -Prelease-sign-artifacts -Pscala-2.12
 git co .
