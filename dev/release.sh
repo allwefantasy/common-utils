88d195b8192e77d513a6bdb1086b456f9006c7a5
WilliamZhu <allwefantasy@gmail.com>
2023-08-22 10:21:17 +0800

    upgrade to 1.0.0

--- dev/release.sh
+++ dev/release.sh
@@ -1,7 +1,7 @@
-git co master
-mlsql_plugin_tool  scala211
-mvn clean deploy -DskipTests -Pdisable-java8-doclint -Prelease-sign-artifacts
+#git co master
+#mlsql_plugin_tool  scala211
+#mvn clean deploy -DskipTests -Pdisable-java8-doclint -Prelease-sign-artifacts
 
-mlsql_plugin_tool  scala212
+#mlsql_plugin_tool  scala212
 mvn clean deploy -DskipTests -Pdisable-java8-doclint -Prelease-sign-artifacts
-git co .
+#git co .
